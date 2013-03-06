ccfff486a64cd91b3300983e320aec49d99b0cf5
belen-mapplas <belen@mapplas.com>
2013-03-06 10:45:53 +0100

    Added navigation bar into app delegate class. In SCAppUtils class commented navBar background code and only set brownie tint color.

---
+++ com.digitalCalc/com.digitalCalc/Classes/Utils/SCAppUtils.h
@@ -0,0 +1,16 @@
+//
+//  SCAppUtils.h
+//  Mapplas
+//
+//  Created by Belén  on 17/01/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#define digitalCalculatorNavBarImageTag 6183746
+#define digitalCalculatorNavBarColor [UIColor colorWithRed:0.66 green:0.33 blue:0.00 alpha:1.0]
+
+@interface SCAppUtils : NSObject
+
++ (void)customizeNavigationController:(UINavigationController *)navController;
+
+@end
