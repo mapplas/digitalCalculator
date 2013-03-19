a34478d09d4feb88b3fd95f7496802d4d95234c9
belen-mapplas <belen@mapplas.com>
2013-03-19 12:51:52 +0100

    Finished ranking screen design for iPhone and iPad.

--- com.digitalCalc/com.digitalCalc/RankingViewController.h
+++ com.digitalCalc/com.digitalCalc/RankingViewController.h
@@ -16,17 +16,23 @@
 }
 
 @property (nonatomic, strong) IBOutlet UILabel *rankingLabel;
+@property (nonatomic, strong) IBOutlet UIView *rankingAlphaView;
 
 @property (nonatomic, strong) IBOutlet UILabel *firstClasifName;
 @property (nonatomic, strong) IBOutlet UILabel *firstClasifPoints;
+@property (nonatomic, strong) IBOutlet UILabel *firstClasifNumber;
 @property (nonatomic, strong) IBOutlet UILabel *secondClasifName;
 @property (nonatomic, strong) IBOutlet UILabel *secondClasifPoints;
+@property (nonatomic, strong) IBOutlet UILabel *secondClasifNumber;
 @property (nonatomic, strong) IBOutlet UILabel *thirdClasifName;
 @property (nonatomic, strong) IBOutlet UILabel *thirdClasifPoints;
+@property (nonatomic, strong) IBOutlet UILabel *thirdClasifNumber;
 @property (nonatomic, strong) IBOutlet UILabel *fourthClasifName;
 @property (nonatomic, strong) IBOutlet UILabel *fourthClasifPoints;
+@property (nonatomic, strong) IBOutlet UILabel *fourthClasifNumber;
 @property (nonatomic, strong) IBOutlet UILabel *fifthClasifName;
 @property (nonatomic, strong) IBOutlet UILabel *fifthClasifPoints;
+@property (nonatomic, strong) IBOutlet UILabel *fifthClasifNumber;
 
 @property (nonatomic, strong) LayoutPresenter *layoutPresenter;
 
--- com.digitalCalc/com.digitalCalc/RankingViewController.m
+++ com.digitalCalc/com.digitalCalc/RankingViewController.m
@@ -15,8 +15,11 @@
 
 @implementation RankingViewController
 
+@synthesize rankingLabel;
+@synthesize rankingAlphaView;
 @synthesize firstClasifName, secondClasifName, thirdClasifName, fourthClasifName, fifthClasifName;
 @synthesize firstClasifPoints, secondClasifPoints, thirdClasifPoints, fourthClasifPoints, fifthClasifPoints;
+@synthesize firstClasifNumber, secondClasifNumber, thirdClasifNumber, fourthClasifNumber, fifthClasifNumber;
 
 @synthesize layoutPresenter = _layoutPresenter;
 
@@ -30,10 +33,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"nav_bar_ranking_left_button_title", @"Ranking controller left button title") style:UIBarButtonItemStyleBordered target:self action:@selector(pop)];
     
     [self initRankingLabels];
+    [self initLayout];
     
     RankingRowTable *table = [[RankingRowTable alloc] init];
     NSMutableDictionary *ranking = [table getFirstBestPuntuations:RANKING_ELEMENTS];
@@ -78,20 +80,56 @@
     [self.navigationController dismissModalViewControllerAnimated:YES];
 }
 
+- (void)initLayout {
+    // Play again nav button
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"nav_bar_ranking_left_button_title", @"Ranking controller left button title") style:UIBarButtonItemStyleBordered target:self action:@selector(pop)];
+    
+    [self.navigationItem.leftBarButtonItem setBackgroundImage:[UIImage imageNamed:@"btn_menu_up.png"] forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
+    [self.navigationItem.leftBarButtonItem setBackgroundImage:[UIImage imageNamed:@"btn_menu_hover.png"] forState:UIControlStateHighlighted barMetrics:UIBarMetricsDefault];
+
+    
+    // Ranking header
+    self.rankingLabel.text = NSLocalizedString(@"ranking_screen_title", @"Ranking screen title");
+    self.rankingLabel.font = [UIFont fontWithName:@"Blokletters Potlood" size:self.rankingLabel.font.pointSize];
+    
+    // Alpha view
+    self.rankingAlphaView.layer.cornerRadius = 20;
+    self.rankingAlphaView.layer.masksToBounds = YES;
+}
+
 - (void)initRankingLabels {
+    int nameLabelSize = 25;
+    
     NSString *userString = NSLocalizedString(@"ranking_screen_no_user_text", @"Ranking screen no user text");
     self.firstClasifName.text = userString;
+    self.firstClasifName.font = [UIFont fontWithName:@"The Girl Next Door" size:nameLabelSize];
     self.secondClasifName.text = userString;
+    self.secondClasifName.font = [UIFont fontWithName:@"The Girl Next Door" size:nameLabelSize];
     self.thirdClasifName.text = userString;
+    self.thirdClasifName.font = [UIFont fontWithName:@"The Girl Next Door" size:nameLabelSize];
     self.fourthClasifName.text = userString;
+    self.fourthClasifName.font = [UIFont fontWithName:@"The Girl Next Door" size:nameLabelSize];
     self.fifthClasifName.text = userString;
+    self.fifthClasifName.font = [UIFont fontWithName:@"The Girl Next Door" size:nameLabelSize];
     
+    int pointLabelSize = 35;
     NSString *pointsString = NSLocalizedString(@"ranking_screen_no_points_text", @"Ranking screen no points text");
     self.firstClasifPoints.text = pointsString;
+    self.firstClasifPoints.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
     self.secondClasifPoints.text = pointsString;
+    self.secondClasifPoints.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
     self.thirdClasifPoints.text = pointsString;
+    self.thirdClasifPoints.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
     self.fourthClasifPoints.text = pointsString;
+    self.fourthClasifPoints.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
     self.fifthClasifPoints.text = pointsString;
+    self.fifthClasifPoints.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
+    
+    self.firstClasifNumber.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
+    self.secondClasifNumber.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
+    self.thirdClasifNumber.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
+    self.fourthClasifNumber.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
+    self.fifthClasifNumber.font = [UIFont fontWithName:@"The Girl Next Door" size:pointLabelSize];
 }
 
 @end
