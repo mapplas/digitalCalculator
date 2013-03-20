5eecaa3adc902712ee381e88e96f4ef60638041d
belen-mapplas <belen@mapplas.com>
2013-03-20 10:02:20 +0100

    Created medium level class.

---
+++ com.digitalCalc/com.digitalCalc/Classes/Levels/MediumMultLevel.h
@@ -0,0 +1,13 @@
+//
+//  MediumMultLevel.h
+//  com.digitalCalc
+//
+//  Created by Belén  on 20/03/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import "Constants.h"
+
+@interface MediumMultLevel : NSObject
+
+@end
