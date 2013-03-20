dced435143d21474132f504941e84068fa913573
belen-mapplas <belen@mapplas.com>
2013-03-20 16:36:19 +0100

    Added custom uislider code.

---
+++ com.digitalCalc/com.digitalCalc/Classes/CustomSlider.h
@@ -0,0 +1,19 @@
+//
+//  CustomSlider.h
+//  com.digitalCalc
+//
+//  Created by Belén  on 20/03/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MNESliderValuePopupView;
+
+@interface CustomSlider : UISlider {
+    MNESliderValuePopupView *valuePopupView;
+}
+
+@property (nonatomic, readonly) CGRect thumbRect;
+
+@end
